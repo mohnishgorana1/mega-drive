b6607120bcf1f2b7a366c47ba3d8565c8064d836
mohnishgorana1 <mohnishgorana1@gmail.com>
2024-09-08 14:00:45 +0530

    middleware.ts checking

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,3 @@
-// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-
-// const protectedRoutes = createRouteMatcher([
-//     '/',
-//     "/favourites",
-//     "/trash",
-//     "/folder"
-// ])
-// export default clerkMiddleware((auth, req) => {
-//     if (protectedRoutes(req)) {
-//         auth().protect();
-//     }
-// });
-
-// export const config = {
-//     matcher: [
-//         '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-//         '/(api|trpc)(.*)',
-//     ],
-// };
-
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 import { NextResponse } from 'next/server';
 
@@ -26,7 +5,6 @@ import { NextResponse } from 'next/server';
 const isPublicRoute = createRouteMatcher([
     "/sign-in",
     "/sign-up",
-    "/",
     "/api/clerk-webhook"
 ])
 
